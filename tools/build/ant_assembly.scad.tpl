ddcfe5023274aae12cab0d338524ce9ce8f47ee7
Free Beachler <fbeachler@gmail.com>
2015-04-18 16:48:21 -0600

    added build system to repo with CC-NC-NA license;

---
+++ tools/build/ant_assembly.scad.tpl
@@ -0,0 +1,6 @@
+// SCAD assembly template for ANT
+// auto-generated - DO NOT edit manually!
+
+use <00str00der.scad>
+
+!scad.method!(bowden = !scad.bowden!, filament_hole = !scad.filament_hole!, bigP = !scad.bigP!, smallP=!scad.smallP!, belt_len=!scad.belt_len!);
---
+++ tools/build/build_all_parts.sh
@@ -0,0 +1,8 @@
+ant build-filtered-assembly-scad -DprofileId=profile-ddrive-60TBP-16TSP-88TB
+ant build-filtered-assembly-scad -DprofileId=profile-ddrive-60TBP-16TSP-90TB
+ant build-filtered-assembly-scad -DprofileId=profile-ddrive-65TBP-17TSP-88TB
+ant build-filtered-assembly-scad -DprofileId=profile-ddrive-65TBP-17TSP-90TB
+ant build-filtered-assembly-scad -DprofileId=profile-bowden-60TBP-16TSP-88TB
+ant build-filtered-assembly-scad -DprofileId=profile-bowden-60TBP-16TSP-90TB
+ant build-filtered-assembly-scad -DprofileId=profile-bowden-65TBP-17TSP-88TB
+ant build-filtered-assembly-scad -DprofileId=profile-bowden-65TBP-17TSP-90TB
